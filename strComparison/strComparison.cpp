0fcf4843569197dd20bf74ad3c42c4f4db6d7328
Badabum <Badabum@users.noreply.github.com>
2014-03-08 00:17:55 +0200

    Update strComparison.cpp

--- strComparison/strComparison.cpp
+++ strComparison/strComparison.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <initializer_list>
 #include <algorithm>
@@ -8,8 +7,8 @@
 #include <locale>
 //#include <cwctype>
 using std::string;
-const char rus[] = "rus_rus.866";
-const char eng[] = "";
+//const char rus[] = "rus_rus.866";
+//const char eng[] = "";
 void arrOut( double**array,int rows,int columns)
 {
 	for (int i = 0; i < rows; i++)
@@ -22,53 +21,6 @@ void arrOut( double**array,int rows,int columns)
 	}
 }
 
-double StrComparison(std::string str1, std::string str2)
-{
-	str1 = " " + str1;
-	str2 = " " + str2;
-	const int rows = str1.length();
-	const int columns = str2.length();
-	const double maxLen = (str1.length() > str2.length()) ? str1.length() : str2.length();
-	double **array = new double*[rows+1]; //matrix which contains all operations
-	double cost = { 0 }, distance = { 0 };
-	for (int i = 0; i < rows+1; i++)
-	{
-		array [i]= new double[columns+1];
-	}
-	for (int i = 0; i < rows + 1;i++)
-	{
-		for (int j = 0; j < columns + 1;j++)
-		{
-			array[i][j] = 0;
-		}
-	}
-	for (int i = 0; i <str1.length()+1; i++)
-		array[i][0] = i;
-	for (int j = 0; j <str2.length()+1; j++)
-		array[0][j] = j;
-	for (int i = 1; i <=str1.length(); i++)
-	{
-		for (int j = 1; j <=str2.length();j++)
-		{
-			if (str1[i] == str2[j])
-			{
-				cost = 0;
-			}
-			else if (tolower(str1[i]) == tolower(str2[j]))
-				cost = 0.5;
-			else if (str1[i] != str2[j])
-				cost = 1;
-			array[i][j] = std::min({ array[i - 1][ j] + 1, array[i][ j - 1] + 1, array[i - 1][ j - 1] + cost });
-			if ((i>1 && j>1) && (str1[i] == str2[j - 1] && str1[i - 1] == str2[j]))
-				array[i][ j] = std::min({ array[i][j], array[i - 2][ j - 2] + cost });
-
-		}
-	}
-	distance = array[str1.length()][ str2.length()];
-	for (int i = 0; i < str1.length() + 1; i++)
-		delete [] array[i];
-	delete[]array;
-	return (maxLen-distance)/maxLen;
 }
 double StrComparison(std::wstring str1, std::wstring str2)
 {
